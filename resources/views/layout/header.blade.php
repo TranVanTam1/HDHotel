62df573a5543512fa052644513250cb8cfe69685
TranVanTam1~ <vantamtran1233@gmail.com>
2024-08-26 15:59:38 +0700

    first commit

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,72 @@
+<section class="header">
+    <div class="header_contact">
+        <div class="container container_contact">
+            <div class="header_phone">
+                <a class="phone_link" href="#">
+                    <i class="fas fa-phone-alt"></i>0242 242 0777
+                </a>
+                <span>
+                    <i class="fas fa-map-marker-alt"></i>
+                    Số 147 Mai Dịch, Cầu Giấy, H&#224; Nội
+                </span>
+            </div>
+            <div class="header_social">
+                <a href="#">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#">
+                    <i class="fab fa-dribbble"></i>
+                </a>
+                <a href="#">
+                    <i class="fab fa-google-plus-g"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="header_nav">
+        <div class="container nav_container">
+            <div class="nav_logo_menu">
+                <div class="nav_logo">
+                    <img src="/images/img/logo-dark.png" alt="No picture">
+                </div>
+                
+            </div>
+            <div class="nav">
+                <div class="nav_menu">
+                    <ul class="menu_list">
+
+                                    <li class="list_item"><a href="/">Trang chủ</a></li>
+                                    <li class="list_item"><a href="/Gioi-thieu">Giới thiệu</a></li>
+                                <li class="list_item">
+                                    <a href="/phong-nghi">Ph&#242;ng nghỉ</a>
+
+                                    <ul class="sub_menu">
+                                                                    <li class="sub_menu-item"><a href="/phong-nghi/13/phong-senior">Ph&#242;ng Senior</a></li>
+                                                                    <li class="sub_menu-item"><a href="/phong-nghi/12/phong-triple">Ph&#242;ng Triple</a></li>
+                                                                    <li class="sub_menu-item"><a href="/phong-nghi/1013/phong-connecting">Ph&#242;ng Connecting</a></li>
+                                                                    <li class="sub_menu-item"><a href="/phong-nghi/2014/phong-deluxe">Ph&#242;ng Deluxe</a></li>
+
+                                    </ul>
+                                </li>
+                                    <li class="list_item"><a href="/dich-vu">Dịch vụ</a></li>
+                                    <li class="list_item"><a href="/tin-tucs">Tin tức</a></li>
+                                    <li class="list_item"><a href="/gallery">H&#236;nh ảnh</a></li>
+                                    <li class="list_item"><a href="/lien-he1">Li&#234;n hệ</a></li>
+
+
+                    </ul>
+                </div>
+            </div>
+            <div class="menu_mobile">
+                <i class="fas fa-bars"></i>
+            </div>
+            
+            
+            <div class="nav_button">
+                <a href="https://booking-guarantee.com/Home/SelectDate?code=WHT8888&CheckIn=&CheckOut=&Adult=Adults&Child=Children&hotelKey=IIT">  <button>Đặt ph&#242;ng ngay</button></a>
+
+            </div>
+        </div>
+    </div>
+</section>
+
